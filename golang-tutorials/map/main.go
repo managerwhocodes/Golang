bfc5fa962e9b970962928d3b0d37da78e4ee32e8
managerwhocodes <managerwhocodes@gmail.com>
2020-04-16 23:12:54 +0530

    More ways to create map

--- golang-tutorials/map/main.go
+++ golang-tutorials/map/main.go
@@ -4,9 +4,19 @@ import "fmt"
 
 func main() {
 	// declare the type of key and value in a map
-	colors := map[string]string{
+	colorsOne := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
 	}
-	fmt.Println(colors)
+	fmt.Println(colorsOne)
+
+	// Some other ways to create map
+	var colorsTwo map[string]string
+	colorsThree := make(map[string]string)
+
+	fmt.Println(colorsTwo)
+	fmt.Println(colorsThree)
+
+	colorsThree["white"] = "#ffffff"
+	fmt.Println(colorsThree)
 }
