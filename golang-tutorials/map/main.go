bf62c044c2a197b3cc311f7075d2bbd92723ae4d
managerwhocodes <managerwhocodes@gmail.com>
2020-04-16 23:07:43 +0530

    Create a map

---
+++ golang-tutorials/map/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare the type of key and value in a map
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+	}
+	fmt.Println(colors)
+}
