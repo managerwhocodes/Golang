0c316a74599bca7beafa73a5dc93de5ee1deb79b
managerwhocodes <managerwhocodes@gmail.com>
2020-04-23 19:32:38 +0530

    Use function to return nil in place of stdout

--- golang-tutorials/http/main.go
+++ golang-tutorials/http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	respOne, err := http.Get("https://www.google.com/")
 	if err != nil {
@@ -31,4 +33,20 @@ func main() {
 
 	// Using writer interface
 	io.Copy(os.Stdout, respTwo.Body)
+
+	fmt.Println("######################################")
+
+	respThree, err := http.Get("https://www.google.com/")
+	if err != nil {
+		fmt.Println("Error : ", err)
+		os.Exit(1)
+	}
+
+	lw := logWriter{}
+	io.Copy(lw, respThree.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	return 1, nil
 }
